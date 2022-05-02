951325af9efe6c742ed3f9742cc4f576f294f3fa
Rachid <rachid.rhazid@lepoles.org>
2022-05-02 04:12:31 +0200

    02.05

---
+++ js/projet.js
@@ -0,0 +1,7 @@
+// pour le titre page1 "annimation du titre" //
+var typed = new Typed(".typing", {
+    strings: ["Developeur", "Designeur","Photographe", "Blogger", "Youtubeur"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
